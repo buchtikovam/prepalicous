26ce632d113121bce4f6efb3b758b7a209e159df
Monika Buchtíková <monika.buchtikova@albi.cz>
2026-09-10 00:25:03 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /><title>prepalicous </title></svelte:head>
+
+{@render children()}
